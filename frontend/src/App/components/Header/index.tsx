2d447034a9af04710b5ef585a4dde0034ba9eee4
MrSterdy <mrsterdy@gmail.com>
2023-01-05 16:47:25 +0400

    Add header

---
+++ frontend/src/App/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import "./index.css";
+
+export default function Header() {
+    return (
+        <header>
+            <object data="icons/logo.svg"></object>
+            
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Account</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
