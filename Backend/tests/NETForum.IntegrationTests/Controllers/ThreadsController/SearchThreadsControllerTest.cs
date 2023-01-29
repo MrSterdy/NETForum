a2b1a0c76dfc9a3b5889392b13ccc66dd0ee270c
MrSterdy <mrsterdy@gmail.com>
2023-01-29 16:49:08 +0400

    backend: add thread search test

---
+++ Backend/tests/NETForum.IntegrationTests/Controllers/ThreadsController/SearchThreadsControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using NETForum.Infrastructure.Database.Entities;
+using NETForum.Models.Responses;
+
+using FluentAssertions;
+
+namespace NETForum.IntegrationTests.Controllers.ThreadsController;
+
+public class SearchThreadsControllerTest : ThreadsControllerTest
+{
+    protected override string Endpoint => base.Endpoint + "/Search";
+
+    public SearchThreadsControllerTest(BackendFactory factory) : base(factory)
+    {
+    }
+    
+    [Fact]
+    public async void Search_Ok()
+    {
+        // Arrange
+        var thread = await Factory.DbManager.Seeder.SeedThreadAsync();
+        var search = thread.Title[..5];
+        
+        // Act
+        using var client = Factory.CreateClient();
+        using var response = await client.GetAsync(Endpoint + $"?title={search}&page=1");
+        
+        // Assert
+        response.EnsureSuccessStatusCode();
+
+        var result = await ParseResponse<Page<ThreadResponse>>(response);
+        result.IsLast.Should().BeTrue();
+        result.Items.Should().NotBeEmpty();
+    }
+}
