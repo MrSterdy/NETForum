19f1231829c23e255f6f970db3892b3c2b969e23
MrSterdy <mrsterdy@gmail.com>
2023-02-02 17:27:46 +0400

    backend: merge repository files with their interface files

--- Backend/src/NETForum.Infrastructure/Database/Repositories/ICommentRepository.cs
+++ Backend/src/NETForum.Infrastructure/Database/Repositories/ICommentRepository.cs
@@ -1,5 +1,7 @@
 ﻿using NETForum.Infrastructure.Database.Entities;
 
+using Microsoft.EntityFrameworkCore;
+
 namespace NETForum.Infrastructure.Database.Repositories;
 
 public interface ICommentRepository : IRepository<Comment>
@@ -7,4 +9,66 @@ public interface ICommentRepository : IRepository<Comment>
     Task<Page<Comment>> GetByPageAsync(int page, int threadId);
 
     Task<int> GetCountByThreadIdAsync(int threadId);
+}
+
+public class CommentRepository : ICommentRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public CommentRepository(ApplicationDbContext context) =>
+        _context = context;
+
+    public async Task<IEnumerable<Comment>> GetAllAsync() =>
+        await _context.Comments
+            .Include(c => c.User)
+            .ToListAsync();
+
+    public async Task<Comment?> GetByIdAsync(int id) =>
+        await _context.Comments
+            .Include(c => c.User)
+            .SingleOrDefaultAsync(c => c.Id == id);
+
+    public async Task<Page<Comment>> GetByPageAsync(int page, int threadId)
+    {
+        if (page <= 0)
+            return Page<Comment>.Empty;
+
+        var skipped = _context.Comments
+            .Where(c => c.ThreadId == threadId)
+            .OrderByDescending(c => c.CreatedDate)
+            .Skip((page - 1) * Constants.PageSize);
+        var isLast = Math.Ceiling((await skipped.CountAsync() - Constants.PageSize) / (float) Constants.PageSize) < 1;
+
+        return new Page<Comment>(await skipped
+            .Take(Constants.PageSize)
+            .Include(c => c.User)
+            .ToListAsync(), isLast);
+    }
+
+    public async Task<int> GetCountByThreadIdAsync(int threadId) =>
+        await _context.Comments.Where(c => c.ThreadId == threadId).CountAsync();
+
+    public async Task<bool> Exists(int id) =>
+        await _context.Comments.AnyAsync(c => c.Id == id);
+
+    public async Task DeleteAsync(Comment entity)
+    {
+        _context.Comments.Remove(entity);
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Comment entity)
+    {
+        _context.Comments.Update(entity);
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task AddAsync(Comment entity)
+    {
+        _context.Comments.Add(entity);
+
+        await _context.SaveChangesAsync();
+    }
 }
--- Backend/src/NETForum.Infrastructure/Database/Repositories/IThreadRepository.cs
+++ Backend/src/NETForum.Infrastructure/Database/Repositories/IThreadRepository.cs
@@ -1,9 +1,85 @@
 ﻿using NETForum.Infrastructure.Database.Entities;
 using Thread = NETForum.Infrastructure.Database.Entities.Thread;
 
+using Microsoft.EntityFrameworkCore;
+
 namespace NETForum.Infrastructure.Database.Repositories;
 
 public interface IThreadRepository : IRepository<Thread>
 {
     Task<Page<Thread>> SearchAsync(int page, int[] tagIds, int? userId, string? title);
+}
+
+public class ThreadRepository : IThreadRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public ThreadRepository(ApplicationDbContext context) =>
+        _context = context;
+
+    public async Task<IEnumerable<Thread>> GetAllAsync() =>
+        await _context.Threads
+            .Include(t => t.User)
+            .ToListAsync();
+
+    public async Task<Thread?> GetByIdAsync(int id) =>
+        await _context.Threads
+            .Include(t => t.User)
+            .Include(t => t.Tags)
+                .ThenInclude(t => t.Tag)
+            .SingleOrDefaultAsync(t => t.Id == id);
+
+    public async Task<Page<Thread>> SearchAsync(int page, int[] tagIds, int? userId, string? title)
+    {
+        if (page <= 0)
+            return Page<Thread>.Empty;
+
+        var threads = _context.Threads.AsQueryable();
+
+        if (userId is not null)
+            threads = threads.Where(t => t.UserId == userId);
+
+        if (title is not null)
+            threads = threads.Where(t => t.Title.Contains(title));
+
+        if (tagIds.Length != 0)
+            threads = threads
+                .Where(t => !t.Tags.All(threadTag => tagIds.Contains(threadTag.TagId))); // ???
+
+        threads = threads
+            .OrderByDescending(t => t.CreatedDate)
+            .Skip((page - 1) * Constants.PageSize);
+        var isLast = Math.Ceiling((await threads.CountAsync() - Constants.PageSize) / (float) Constants.PageSize) < 1;
+
+        return new Page<Thread>(await threads
+            .Take(Constants.PageSize)
+            .Include(t => t.User)
+            .Include(t => t.Tags)
+                .ThenInclude(t => t.Tag)
+            .ToListAsync(), isLast);
+    }
+
+    public async Task<bool> Exists(int id) =>
+        await _context.Threads.AnyAsync(t => t.Id == id);
+
+    public async Task DeleteAsync(Thread entity)
+    {
+        _context.Threads.Remove(entity);
+        
+        await _context.SaveChangesAsync();
+    }
+    
+    public async Task UpdateAsync(Thread entity)
+    {
+        _context.Threads.Update(entity);
+        
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task AddAsync(Thread entity)
+    {
+        _context.Threads.Add(entity);
+
+        await _context.SaveChangesAsync();
+    }
 }
